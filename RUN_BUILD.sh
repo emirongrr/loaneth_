164123ca8c490c457bf927b40ee390315c31b3fa
delledev <lucadelle2@gmail.com>
2023-06-16 22:08:02 +0300

    added script files for running the project

---
+++ RUN_BUILD.sh
@@ -0,0 +1,3 @@
+npm i --legacy-peer-deps && npm run build && npm run start
+echo 'SERVER SHUT DOWN Press any key to continue...'
+read
---
+++ RUN_DEV.sh
@@ -0,0 +1,3 @@
+npm i --legacy-peer-deps && npm run dev
+echo 'SERVER SHUT DOWN Press any key to continue...'
+read
